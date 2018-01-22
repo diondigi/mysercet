940d1c2a3cce5c827d9b182e61addd61813083e5
diondigi <417144633@qq.com>
2018-01-22 17:30:27 +0800

    add hello.py

--- hello.py
+++ hello.py
@@ -3,11 +3,30 @@
 #__auther__:Administrator
 # date : 2018/1/22
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
+#
+# @app.route('/')
+# def index():
+#     return '<h1>Hello fan!</h1>'
+#
+@app.route('/user/<name>')
+def user(name):
+    return '<h2>Hello,%s</h2>' % name
+
+
+
 
 @app.route('/')
 def index():
-    return '<h1>Hello world!</h1>'
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your brower is %s</p>' % user_agent
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+#test part
+test = app.url_map
+print(test)
