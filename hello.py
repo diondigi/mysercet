93029bd6c80479a9c5865b5e43c9d2b5c085485d
diondigi <417144633@qq.com>
2018-01-22 23:50:41 +0800

    add hello.py

--- hello.py
+++ hello.py
@@ -2,31 +2,36 @@
 #---coding:utf-8---
 #__auther__:Administrator
 # date : 2018/1/22
-from flask import Flask
-from flask import request
+from flask import Flask,render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-#
-# @app.route('/')
-# def index():
-#     return '<h1>Hello fan!</h1>'
-#
-@app.route('/user/<name>')
-def user(name):
-    return '<h2>Hello,%s</h2>' % name
-
+bootstrap = Bootstrap(app)
 
 
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<p>Your brower is %s</p>' % user_agent
+    return render_template('index.html')
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+
+
+# @app.route('/')
+# def index():
+#     user_agent = request.headers.get('User-Agent')
+#     return '<p>Your brower is %s</p>' % user_agent
+
+if __name__ == '__main__':
+    # manager.run()
+    app.run(debug=True)
+
 
 
 #test part
-test = app.url_map
-print(test)
+# test = app.url_map
+# print(test)
