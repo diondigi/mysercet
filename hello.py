e151938979db0ea5eef5289fe8ef92599b0c8650
diondigi <417144633@qq.com>
2018-01-23 21:51:38 +0800

    add hello.py

--- hello.py
+++ hello.py
@@ -4,19 +4,48 @@
 # date : 2018/1/22
 from flask import Flask,render_template
 from flask_bootstrap import Bootstrap
+from flask_script import Manager
+# from flask_moment import Moment
+# from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import data_required
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators = [data_required()])
+    submit = SubmitField('Submit')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
+# manager = Manager(app)
+# moment = Moment(app)
 
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    name = None
+    nameForm = NameForm()
+    if nameForm.validate_on_submit():
+        name = nameForm.name.data
+        nameForm.name.data = ''
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+    return render_template('index.html', form=nameForm, name=name)
+
+
+# @app.route('/user/<name>')
+# def user(name):
+#     return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_net_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_net_found(e):
+    return render_template('500.html'), 500
+
 
 
 
